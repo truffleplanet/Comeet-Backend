900324300be9d367fdf49b1d14d4b874bc2d1448
SeunghunKang <kangseunghun9927@gmail.com>
2026-05-29 10:32:42 +0900

    refactor: 공통 예외 처리와 로깅 기준 정리

--- src/main/java/com/backend/common/error/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/backend/common/error/exception/handler/GlobalExceptionHandler.java
@@ -4,27 +4,15 @@ import java.sql.SQLException;
 
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.backend.common.error.ErrorCode;
-import com.backend.common.error.exception.AiException;
-import com.backend.common.error.exception.AuthException;
-import com.backend.common.error.exception.BeanException;
 import com.backend.common.error.exception.BusinessException;
-import com.backend.common.error.exception.FileException;
-import com.backend.common.error.exception.FlavorException;
-import com.backend.common.error.exception.MenuException;
-import com.backend.common.error.exception.PassportException;
-import com.backend.common.error.exception.ReviewException;
-import com.backend.common.error.exception.RoasteryException;
-import com.backend.common.error.exception.S3FileException;
-import com.backend.common.error.exception.StoreException;
-import com.backend.common.error.exception.UserException;
-import com.backend.common.error.exception.VisitException;
 import com.backend.common.response.BaseResponse;
 import com.backend.common.response.ErrorResponse;
 import com.backend.common.util.LoggingUtil;
@@ -37,33 +25,12 @@ import lombok.extern.slf4j.Slf4j;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleException(Exception ex, HttpServletRequest request) {
-		LoggingUtil.logException("지정되지 않은 예외 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, request);
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(BaseResponse.fail(response));
-	}
-
-	@ExceptionHandler(SQLException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleSQLException(SQLException ex, HttpServletRequest request) {
-		LoggingUtil.logException("SQLException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ErrorCode.DATABASE_ERROR, request);
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(BaseResponse.fail(response));
-	}
-
-	@ExceptionHandler(JwtException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleJwtException(JwtException ex, HttpServletRequest request) {
-		LoggingUtil.logException("JwtException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_TOKEN, request);
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(BaseResponse.fail(response));
-	}
-
 	@ExceptionHandler(BusinessException.class)
 	public ResponseEntity<BaseResponse<ErrorResponse>> handleBusinessException(
 		BusinessException ex,
 		HttpServletRequest request
 	) {
-		LoggingUtil.logException("BusinessException 발생", ex, request);
+		LoggingUtil.logBusinessException(ex, request);
 		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
 		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
 	}
@@ -76,7 +43,7 @@ public class GlobalExceptionHandler {
 		LoggingUtil.logValidationException(ex, request);
 		ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT, request);
 		response.addValidationErrors(ex.getBindingResult());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(BaseResponse.fail(response));
+		return ResponseEntity.status(ErrorCode.INVALID_INPUT.getHttpStatus()).body(BaseResponse.fail(response));
 	}
 
 	@ExceptionHandler(DataAccessException.class)
@@ -84,9 +51,9 @@ public class GlobalExceptionHandler {
 		DataAccessException ex,
 		HttpServletRequest request
 	) {
-		LoggingUtil.logException("DataAccessException 예외 발생", ex, request);
+		LoggingUtil.logSystemException("DataAccessException", ex, request);
 		ErrorResponse response = ErrorResponse.of(ErrorCode.DATABASE_ERROR, request);
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(BaseResponse.fail(response));
+		return ResponseEntity.status(ErrorCode.DATABASE_ERROR.getHttpStatus()).body(BaseResponse.fail(response));
 	}
 
 	@ExceptionHandler(DuplicateKeyException.class)
@@ -94,137 +61,36 @@ public class GlobalExceptionHandler {
 		DuplicateKeyException ex,
 		HttpServletRequest request
 	) {
-		LoggingUtil.logException("DuplicateKeyException 예외 발생", ex, request);
+		LoggingUtil.logSystemException("DuplicateKeyException", ex, request);
 		ErrorResponse response = ErrorResponse.of(ErrorCode.DUPLICATED_KEY, request);
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(BaseResponse.fail(response));
+		return ResponseEntity.status(ErrorCode.DUPLICATED_KEY.getHttpStatus()).body(BaseResponse.fail(response));
 	}
 
-	@ExceptionHandler(UserException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleUserException(
-		UserException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("UserException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
+	@ExceptionHandler({NoHandlerFoundException.class, NoResourceFoundException.class})
+	public ResponseEntity<BaseResponse<ErrorResponse>> handleEndpointNotFound(Exception ex, HttpServletRequest request) {
+		LoggingUtil.logNotFoundException(ex, request);
+		ErrorResponse response = ErrorResponse.of(ErrorCode.NOT_FOUND, request);
+		return ResponseEntity.status(ErrorCode.NOT_FOUND.getHttpStatus()).body(BaseResponse.fail(response));
 	}
 
-	@ExceptionHandler(AuthException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleAuthException(
-		AuthException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("AuthException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
+	@ExceptionHandler(SQLException.class)
+	public ResponseEntity<BaseResponse<ErrorResponse>> handleSQLException(SQLException ex, HttpServletRequest request) {
+		LoggingUtil.logSystemException("SQLException", ex, request);
+		ErrorResponse response = ErrorResponse.of(ErrorCode.DATABASE_ERROR, request);
+		return ResponseEntity.status(ErrorCode.DATABASE_ERROR.getHttpStatus()).body(BaseResponse.fail(response));
 	}
 
-	@ExceptionHandler(VisitException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleVisitException(
-		VisitException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("VisitException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
+	@ExceptionHandler(JwtException.class)
+	public ResponseEntity<BaseResponse<ErrorResponse>> handleJwtException(JwtException ex, HttpServletRequest request) {
+		LoggingUtil.logSystemException("JwtException", ex, request);
+		ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_TOKEN, request);
+		return ResponseEntity.status(ErrorCode.INVALID_TOKEN.getHttpStatus()).body(BaseResponse.fail(response));
 	}
 
-	@ExceptionHandler(ReviewException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleReviewException(
-		ReviewException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("ReviewException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
-	}
-
-	// ====== 아래부터 누락된 Exception 추가 ======
-
-	@ExceptionHandler(AiException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleAiException(AiException ex, HttpServletRequest request) {
-		LoggingUtil.logException("AiException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
-	}
-
-	@ExceptionHandler(BeanException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleBeanException(
-		BeanException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("BeanException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
-	}
-
-	@ExceptionHandler(FileException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleFileException(
-		FileException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("FileException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
-	}
-
-	@ExceptionHandler(S3FileException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleS3FileException(
-		S3FileException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("S3FileException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
-	}
-
-	@ExceptionHandler(FlavorException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleFlavorException(
-		FlavorException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("FlavorException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
-	}
-
-	@ExceptionHandler(MenuException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleMenuException(
-		MenuException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("MenuException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
-	}
-
-	@ExceptionHandler(PassportException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handlePassportException(
-		PassportException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("PassportException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
-	}
-
-	@ExceptionHandler(RoasteryException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleRoasteryException(
-		RoasteryException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("RoasteryException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
-	}
-
-	@ExceptionHandler(StoreException.class)
-	public ResponseEntity<BaseResponse<ErrorResponse>> handleStoreException(
-		StoreException ex,
-		HttpServletRequest request
-	) {
-		LoggingUtil.logException("StoreException 발생", ex, request);
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode(), request);
-		return ResponseEntity.status(ex.getErrorCode().getHttpStatus()).body(BaseResponse.fail(response));
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<BaseResponse<ErrorResponse>> handleException(Exception ex, HttpServletRequest request) {
+		LoggingUtil.logSystemException("Unhandled exception", ex, request);
+		ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, request);
+		return ResponseEntity.status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus()).body(BaseResponse.fail(response));
 	}
 }
