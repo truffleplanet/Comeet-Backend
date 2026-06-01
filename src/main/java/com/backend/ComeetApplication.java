59fe985c4e8bf1700bf2f10b6312f4ecd3bb4c35
SeunghunKang <kangseunghun9927@gmail.com>
2026-06-02 02:57:49 +0900

    feat: Flavor 메타데이터 조회 성능 향상을 위한 Spring Cache 캐싱 도입

--- src/main/java/com/backend/ComeetApplication.java
+++ src/main/java/com/backend/ComeetApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties
 @EnableRetry
 @EnableScheduling
+@EnableCaching
 public class ComeetApplication {
 
 	public static void main(String[] args) {
